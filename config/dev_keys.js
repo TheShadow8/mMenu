42cfa32e754381580abd00ac31649952e29218f5
TheShadow8 <35181893+TheShadow8@users.noreply.github.com>
2018-08-14 23:42:02 -0400

    user route done

--- config/dev_keys.js
+++ config/dev_keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://mmenu:mmenu2018@ds221242.mlab.com:21242/mmenu"
+  mongoURI: "mongodb://mmenu:mmenu2018@ds221242.mlab.com:21242/mmenu",
+  secretOrkey: "secret"
 };
--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 
 const app = express();
 
-// DB config
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// DB config
 const db = require("./config/dev_keys").mongoURI;
 
 // Connect to MongoDB
-
 mongoose
   .connect(db)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
 
 // Use routes
 app.use("/api/users", users);
